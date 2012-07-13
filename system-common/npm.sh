ddea7fc2aa6847710b775447ccdb555e2dfac00e
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-07-13 18:20:42 +0200

    Install json2yaml

--- system-common/npm.sh
+++ system-common/npm.sh
@@ -8,6 +8,7 @@ npm install -g codestream
 npm install -g deja
 npm install -g express
 npm install -g jshint
+npm install -g json2yaml
 npm install -g markdown-wiki
 npm install -g node-gyp
 npm install -g pegjs
