b31c9f25dea3846bf4ea5b1773fbf8ebf204fa12
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-07-31 15:40:46 +0200

    Remove ssh-agent-locker

--- system-osx/user-setup.sh
+++ system-osx/user-setup.sh
@@ -23,11 +23,6 @@ mkdir -p "$SUBLIME_PACKAGES"
 rm -rf "$SUBLIME_USER_SETTINGS"
 ln -s $HOME/.sublime "$SUBLIME_USER_SETTINGS"
 
-# Setup ssh-agent-locker
-mkdir -p ~/Library/LaunchAgents
-cp /usr/local/Cellar/ssh-agent-locker/0.1.0/com.seaandco.geoff.ssh-agent-locker.plist ~/Library/LaunchAgents/
-launchctl load -w ~/Library/LaunchAgents/com.seaandco.geoff.ssh-agent-locker.plist
-
 # Disable Adobe Updater for user
 echo '<?xml version="1.0" encoding="UTF-8" ?><Preferences><UpdaterPrefSchedCheckFreq>3</UpdaterPrefSchedCheckFreq><Automatic>3</Automatic></Preferences>' > ~/Library/Application\ Support/Adobe/AAMUpdater/1.0/AdobeUpdaterPrefs.dat
 
