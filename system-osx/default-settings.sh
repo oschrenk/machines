aeba656673ef3ce8fc894795989e6763e7ecc4ac
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-16 19:48:56 +0300

    Ask for the administrator password upfront

--- system-osx/default-settings.sh
+++ system-osx/default-settings.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # Mainly taken from https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 ## --------------------------------------------------------------------------
 ## System
 ## --------------------------------------------------------------------------
