c93f5c8318004a5e1475283e4f4181f45a878129
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-08-16 20:07:30 +0300

    Turn off keyboard illumination when computer is not used for 5 minutes

--- system-osx/default-settings.sh
+++ system-osx/default-settings.sh
@@ -39,6 +39,8 @@ defaults write com.apple.menuextra.battery ShowTime -string "NO"
 
 # Automatically illuminate built-in MacBook keyboard in low light
 defaults write com.apple.BezelServices kDim -bool true
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
 
 ## --------------------------------------------------------------------------
 ## System
