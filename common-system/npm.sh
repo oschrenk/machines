1ab047a64848c5035fed728cf98013512027f27b
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-12-10 19:36:36 +0100

    Remove unused modules

--- common-system/npm.sh
+++ common-system/npm.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 # Install npm
-curl http://npmjs.org/install.sh | sh
+# brew node install npm
+# curl http://npmjs.org/install.sh | sh
 
 # Install global node modules
-npm install -g codestream
 npm install -g deja
 npm install -g express
 npm install -g jshint
 npm install -g json2yaml
 npm install -g markdown-wiki
-npm install -g node-gyp
 npm install -g pegjs
-npm install -g webmake
-
-
