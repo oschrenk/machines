ca05e4df492bdbe0fe53b545029d95f18beacccb
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-12-27 16:55:42 +0100

    Hot corner documentation

--- osx/defaults-user.sh
+++ osx/defaults-user.sh
@@ -103,11 +103,21 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# active corner top left: All Windows
+# Hot corners
+# 0  : noop
+# 2  : Mission Control
+# 3  : Application Windows
+# 4  : Desktop
+# 5  : Start screen saver
+# 6  : Disable screen saver
+# 7  : Dashboard
+# 10 : Put Display to sleep
+# 11 : Launchpad
+# active corner top left: Mission Control
 defaults write com.apple.dock wvous-tl-corner -int 2
-# active corner top right: All Windows
+# active corner top right: Mission Control
 defaults write com.apple.dock wvous-tr-corner -int 2
-# active corner bottom left: Aplications Windows
+# active corner bottom left: Application Windows
 defaults write com.apple.dock wvous-bl-corner -int 3
 # active corner bottom right: Desktop
 defaults write com.apple.dock wvous-br-corner -int 4
