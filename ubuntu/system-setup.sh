ffb005e22c06cd83c5119da7cbe25b85f542ba27
Oliver Schrenk <olisch@elmar.nl>
2013-05-06 08:57:39 +0200

    Install git-sh

--- ubuntu/system-setup.sh
+++ ubuntu/system-setup.sh
@@ -9,3 +9,17 @@ sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
 
 # Install npm and the global npm modules
 . ../system-common/npm.sh
+
+# cd Projec
+
+cd $HOME
+mkdir-p  Projects
+cd Projects
+mkdir -p external
+cd external
+
+# Install git-sh
+git clone git://github.com/rtomayko/git-sh.git
+cd git-sh
+make
+sudo make install
