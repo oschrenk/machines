b252ef549f23942635bdde7b5d24ec40a37b85a9
Oliver Schrenk <olisch@elmar.nl>
2013-05-06 09:32:57 +0200

    Configure Sublime Text 2

--- ubuntu/user-setup.sh
+++ ubuntu/user-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-# Setup common user
-. ../user-template/user-setup.sh
+# Symlink sublime user settings
+SUBLIME_PACKAGES="$HOME/.config/sublime-text-2/Packages"
+SUBLIME_USER_SETTINGS="$SUBLIME_PACKAGES/User"
+mkdir -p "$SUBLIME_PACKAGES"
+rm -rf "$SUBLIME_USER_SETTINGS"
+ln -s $HOME/.sublime "$SUBLIME_USER_SETTINGS"
