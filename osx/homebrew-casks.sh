b20abf86f77e2bfb62e925b7b0721b4dc8f0de94
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-11-06 22:20:37 +0100

    Move casks to their own file

---
+++ osx/homebrew-casks.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Install native apps
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# install to global directory
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+function installcask() {
+	sudo brew cask install "${@}" 2> /dev/null
+}
+
+# applications
+installcask bettertouchtool
+installcask bodega
+installcask cheat-sheet
+installcask cocoa-rest-client
+installcask cog
+installcask dash
+installcask dropbox
+installcask eclipse-ide
+installcask evernote
+installcask firefox
+installcask f-lux
+installcask gfxcardstatus
+installcask gimp
+installcask gitx-rowanj
+installcask google-chrome
+installcask handbrake
+installcask hexfiend
+installcask inkscape
+installcask i-stumbler
+installcask intellij-community
+installcask kaleidoscope
+installcask keyboard-cleaner
+installcask libre-office
+installcask marked2
+installcask mou
+installcask mplayerx
+installcask omni-graffle-pro
+installcask onepassword
+installcask onyx
+installcask paparazzi
+installcask papers
+installcask quicksilver
+installcask reggy
+installcask rdio
+installcask sequel-pro
+installcask skype
+installcask spotify
+installcask sublime-text-3
+installcask the-unarchiver
+installcask the-hit-list
+installcask transmission
+installcask virtualbox
+installcask vlc
+installcask xld
