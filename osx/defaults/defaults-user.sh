9947e2da12fd7b570fd42f39fbddf241f00cf0e8
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-05-06 08:04:03 +0200

    St faster key repeat and shorter delay

--- osx/defaults/defaults-user.sh
+++ osx/defaults/defaults-user.sh
@@ -104,6 +104,12 @@ defaults write com.apple.menuextra.battery ShowTime -string "NO"
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
+# Set a keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Set a delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
 ## --------------------------------------------------------------------------
 # Access and Input
 ## --------------------------------------------------------------------------
