80cdc360cec1eef19360ea54341d82230bb8ce84
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-07-02 00:28:56 +0200

    Install all the apps!

--- osx/homebrew/homebrew-casks.sh
+++ osx/homebrew/homebrew-casks.sh
@@ -12,47 +12,57 @@ function installcask() {
 	sudo brew cask install "${@}" 2> /dev/null
 }
 
+# app store
+
+# due
+# data-graph
+# msgfiler
+# one day
+# readkit
+# vox
+# xcode
+
 # applications
+installcask actions-server
 installcask bettertouchtool
-installcask bodega
 installcask cheat-sheet
 installcask cocoa-rest-client
 installcask cog
 installcask dash
 installcask dropbox
-installcask eclipse-ide
 installcask evernote
-installcask firefox
 installcask gfxcardstatus
-installcask gimp
 installcask gitx-rowanj
 installcask google-chrome
 installcask handbrake
 installcask hexfiend
 installcask inkscape
-installcask i-stumbler
-installcask intellij-community
+installcask intellij-idea
 installcask kaleidoscope
 installcask keyboard-cleaner
-installcask libre-office
 installcask marked2
-installcask mou
+installcask mindnode-pro
 installcask mplayerx
 installcask omni-graffle-pro
 installcask onepassword
 installcask onyx
 installcask paparazzi
 installcask papers
+installcask pixelstick
 installcask quicksilver
 installcask reggy
-installcask rdio
 installcask sequel-pro
 installcask skype
+installcask slate
 installcask spotify
 installcask sublime-text-3
+installcask teamviewer
 installcask the-unarchiver
 installcask the-hit-list
 installcask transmission
 installcask virtualbox
+installcask viscosity
 installcask vlc
+installcask vox-preference-pane
+installcask xact
 installcask xld
