e4dce307e9e64a65ee4514114b96debac540c1bd
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-07-02 00:40:18 +0200

    Reason for taps

--- osx/homebrew/homebrew.sh
+++ osx/homebrew/homebrew.sh
@@ -2,8 +2,15 @@
 
 brew tap homebrew/dupes
 brew tap homebrew/binary
-brew tap homebrew/dupes
+
+# ssh agent
+brew tap oschrenk/formulae
+
+# jenv
 brew tap jenv/jenv
+
+# sentaku
+brew tap rcmdnk/rcmdnkpac
 
 ./homebrew-packages.sh
 ./homebrew-casks.sh
