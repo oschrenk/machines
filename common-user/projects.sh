49fa26350534a29e3c5eb7c18506605bfa57f49a
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-07-02 00:44:33 +0200

    Install git-sh via homebrew

--- common-user/projects.sh
+++ common-user/projects.sh
@@ -3,18 +3,7 @@ cd $HOME
 mkdir -p Projects/devops
 mkdir -p Projects/external
 
-
 ## devops projects
 cd $HOME/Projects/devops
 git clone git@github.com:oschrenk/machines.git
 git clone git@github.com:oschrenk/maven-archetype.git
-
-
-## external projects
-
-### Install git-sh
-cd $HOME/Projects/external
-git clone git://github.com/rtomayko/git-sh.git
-cd git-sh
-make
-sudo make install
