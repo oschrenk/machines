4d92d5918e519568ea0bb483911ab2ccb9609af9
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-01-17 17:33:12 +0100

    Disable Spotlight indexing for volume mounted

--- osx/defaults/defaults-system.sh
+++ osx/defaults/defaults-system.sh
@@ -102,6 +102,11 @@ sudo sysctl -w net.inet.tcp.slowstart_flightsize=10
 # Remove Spotlight icon
 # sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
 
+# Disable Spotlight indexing for any volume that gets mounted and has not yet
+# been indexed before.
+# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+
 ## --------------------------------------------------------------------------
 # Time Machine
 ## --------------------------------------------------------------------------
