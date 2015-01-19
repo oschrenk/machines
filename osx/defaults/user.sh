10acfd5e59ce5cd55d9f7b5bf6ac0642be7fe9f9
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-01-19 16:34:06 +0100

    Move terminal settings to different file

--- osx/defaults/user.sh
+++ osx/defaults/user.sh
@@ -402,23 +402,6 @@ defaults write com.apple.mail BundleCompatibilityVersion 4
 ## --------------------------------------------------------------------------
 
 
-## --------------------------------------------------------------------------
-# Terminal
-## --------------------------------------------------------------------------
-
-# Only use UTF-8 in Terminal.app
-defaults write com.apple.terminal StringEncodings -array 4
-
-# Use the IR_Black theme/settings by default in Terminal.app
-open "./settings/IR_Black.terminal"
-sleep 1 # Wait a bit to make sure the theme is loaded
-defaults write com.apple.Terminal "Default Window Settings" -string "IR_Black"
-defaults write com.apple.Terminal "Startup Window Settings" -string "IR_Black"
-
-# Enable “focus follows mouse” for Terminal.app and all X11 apps
-# This means you can hover over a window and start typing in it without clicking first
-#defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true
 
 ###############################################################################
 # Kill affected applications                                                  #
