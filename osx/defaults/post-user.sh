54e1a494d7337ee3261bd3b6b3493293b942e779
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-01-19 16:30:19 +0100

    Move sudo commands to post-user

---
+++ osx/defaults/post-user.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Load new settings before rebuilding the index
+killall mds > /dev/null 2>&1
+# Make sure indexing is enabled for the main volume
+sudo mdutil -i on / > /dev/null
+# Rebuild the index from scratch
+sudo mdutil -E / > /dev/null
+
