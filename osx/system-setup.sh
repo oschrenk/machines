c60cd4814fab1d94e762d8c05dfa1fe57009d0ac
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-07-24 15:26:44 +0200

    Don't install font as admin

--- osx/system-setup.sh
+++ osx/system-setup.sh
@@ -3,11 +3,6 @@
 # Install homebrew package manager
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
-# Install Terminal Font
-wget --directory-prefix=$HOME/Downloads/ https://gist.github.com/raw/1627888/c4e92f81f7956d4ceaee11b5a7b4c445f786dd90/Menlo-ForPowerline.ttc.zip
-unzip ~/Downloads/Menlo-ForPowerline.ttc.zip -d ~/Downloads
-cp ~/Downloads/Menlo-ForPowerline.ttc /Library/Fonts
-
 # osx system defaults
 ./defaults/system.sh
 
