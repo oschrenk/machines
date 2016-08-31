67396f83e7fbb0151c6e7d0e73dc7d968c57d960
Oliver Schrenk <oliver.schrenk@gmail.com>
2016-08-31 09:48:07 +0200

    cask: Install more things

--- macos/homebrew/casks.sh
+++ macos/homebrew/casks.sh
@@ -15,6 +15,9 @@ function installcask() {
 
 # app store
 
+# 1password
+# affinity designer
+# afiinity photo
 # due
 # data-graph
 # msgfiler
@@ -23,14 +26,19 @@ function installcask() {
 # vox
 # xcode
 
+# external
+# docker for mac
 
 # /Applications
+installcask atom
+installcask ealibre
 installcask cog
 installcask cyberduck
 installcask dash
 installcask dropbox
 installcask evernote
 installcask google-chrome
+installcask hammerspoon
 installcask handbrake
 installcask hexfiend
 installcask hipchat
@@ -38,17 +46,21 @@ installcask inkscape
 installcask intellij-idea
 installcask iterm2
 installcask keyboard-cleaner
+installcask knockknock
+installcask lunchy
 installcask marked
 installcask mindnode-pro
 installcask mplayerx
 installcask omni-graffle-pro
 installcask onepassword
+installcask paw
 installcask reggy
 installcask rescuetime
 installcask sequel-pro
 installcask skype
 installcask slack
 installcask teamviewer
+installcask telegram
 installcask the-unarchiver
 installcask the-hit-list
 installcask transmission
