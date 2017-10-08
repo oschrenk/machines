f3c39a5954d2538553bae5c6d56ab24ae1debefa
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-08 13:23:28 +0200

    Create Log dir

--- common-user/projects.sh
+++ common-user/projects.sh
@@ -3,6 +3,9 @@ cd $HOME
 mkdir -p Projects/devops
 mkdir -p Projects/external
 
+# Create Logs
+mkdir -p $HOME/Documents/Log
+
 ## devops projects
 cd $HOME/Projects/devops
 git clone git@github.com:oschrenk/machines.git
