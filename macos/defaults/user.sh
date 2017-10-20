cb81743c56664892c340eaa312f1d04e62468d35
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-20 23:46:44 +0200

    default: Remove redundant click settings

--- macos/defaults/user.sh
+++ macos/defaults/user.sh
@@ -218,10 +218,6 @@ defaults write com.apple.dock wvous-br-corner -int 4
 # true = up is down // natural
 defaults write -g com.apple.swipescrolldirection -bool true
 
-# Enable tap to click (Trackpad) for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
 ## --------------------------------------------------------------------------
 # Trackpad
 ## --------------------------------------------------------------------------
