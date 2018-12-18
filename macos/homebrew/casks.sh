933d27185139eca0424c132b78b525eb78d52260
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-12-18 23:13:28 +0100

    macos, cask: Cleanup unused apps

--- macos/homebrew/casks.sh
+++ macos/homebrew/casks.sh
@@ -11,7 +11,6 @@ brew cask install mplus-powerline
 
 # /Applications
 installcask 1password
-installcask atom
 installcask bettertouchtool
 installcask calibre
 installcask cog
@@ -19,7 +18,6 @@ installcask dash
 installcask docker
 installcask dropbox
 installcask evernote
-installcask flux
 installcask google-chrome
 installcask grammarly
 installcask hammerspoon
@@ -32,21 +30,17 @@ installcask knockknock
 installcask lunchy
 installcask karabiner-elements
 installcask keycastr             # shows key strokes on screen
-installcask little-snitch
 installcask marked
 installcask marp
 installcask mindnode-pro
 installcask mplayerx
 installcask muzzle               # stops os-notifications during presentations
 installcask omnigraffle
-installcask osxfuse
 installcask paparazzi
 installcask paw
 installcask plan
 installcask postman
 installcask reggy
-installcask rescuetime
-installcask robo-3t
 installcask rocket               # quick emoji access
 installcask selfcontrol
 installcask sequel-pro
@@ -61,8 +55,6 @@ installcask transmission
 installcask vlc
 installcask xact
 installcask xld
-
-installcask mendeley-desktop
 
 # Quicklook
 installcask ttscoff-mmd-quicklook
