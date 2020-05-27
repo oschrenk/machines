f8ef6c3acd88a114e5fd5e67f7d564ff0c6329f3
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-05-27 12:33:39 +0200

    macos: Update homebrew install command

--- macos/system-setup.sh
+++ macos/system-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Install homebrew package manager
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 # osx system defaults
 ./defaults/system.sh
 
