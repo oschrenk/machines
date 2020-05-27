3144208a3f7160cfc9c44a8f7d7cfedbea50edca
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-05-27 18:45:00 +0200

    source append/prepend to path to load env

--- 03-dotfiles.sh
+++ 03-dotfiles.sh
@@ -5,6 +5,9 @@ git clone https://github.com/oschrenk/dotfiles $HOME/.tilde/dotfiles
 source $HOME/.tilde/dotfiles/.config/fish/functions/tilde.fish
 tilde link dotfiles
 
+source $HOME/.tilde/dotfiles/.config/fish/functions/append-to-path.fish
+source $HOME/.tilde/dotfiles/.config/fish/functions/prepend-to-path.fish
+
 # setup env
 source $HOME/.tilde/dotfiles/.config/fish/env.fish
 
