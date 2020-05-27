5ad4292ba506d5a3bf4642e45f29e022a26be35a
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-05-27 17:08:14 +0200

    Remove raspberry

--- raspberry/system-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Update
-apt-get update
-apt-get upgrade
-
-# Add the new bash to the list of legit shells
-sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
--- raspberry/user-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Setup common user
-. ../user-template/user-setup.sh
