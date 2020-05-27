a25a55d548ef189546bb1e6e1bf8b84fe6429d4e
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-05-27 16:44:13 +0200

    Remove ubuntu

--- ubuntu/user-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Symlink sublime user settings
-SUBLIME_PACKAGES="$HOME/.config/sublime-text-2/Packages"
-SUBLIME_USER_SETTINGS="$SUBLIME_PACKAGES/User"
-mkdir -p "$SUBLIME_PACKAGES"
-rm -rf "$SUBLIME_USER_SETTINGS"
-ln -s $HOME/.sublime "$SUBLIME_USER_SETTINGS"
