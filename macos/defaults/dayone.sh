5fc899413fab0935169c1956e0a3249459d6fec8
Oliver Schrenk <oliver.schrenk@gmail.com>
2020-05-27 12:47:18 +0200

    macos: Remove dayone defaults

--- macos/defaults/dayone.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-defaults write "$HOME/Library/Group Containers/5U8NS4GX82.dayoneapp/Data/Preferences/dayone.plist" JournalPackageURL "$HOME/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Auto Import/Default Journal.dayone"
