03dd688728a245971b8134733538e342f58b4a60
Oliver Schrenk <oliver.schrenk@gmail.com>
2021-02-20 11:31:30 +0100

    brew: git-lfs

--- installs/packages.sh
+++ installs/packages.sh
@@ -63,6 +63,7 @@ brew install wget
 ## git
 brew install git                           # dvcs
 brew install git-extras                    # nice git extras
+brew install git-lfs                       # large file storage
 brew install diff-so-fancy                 # better looking diffs
 brew install hub                           # interact with github
 brew install zaquestion/tap/lab            # interact with gitlab
