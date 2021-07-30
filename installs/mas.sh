61ad1c4d62562e2f961739a5040f545365081c08
Oliver Schrenk <oliver.schrenk@gmail.com>
2021-07-30 16:01:34 +0200

    mas: MindNode

--- installs/mas.sh
+++ installs/mas.sh
@@ -4,6 +4,7 @@ mas install 824171161   # Affinity Designer
 mas install 824183456   # Affinity Photo
 mas install 1091189122  # Bear
 mas install 409183694   # Keynote
+mas install 1289197285  # MindNode
 mas install 1464122853  # NextDNS
 mas install 1505432629  # NotePlan 3
 mas install 409203825   # Numbers
