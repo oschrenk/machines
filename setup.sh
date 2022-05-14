09efb46cc98155740f08212280c5ed15aa517b78
Oliver Schrenk <oliver.schrenk@gmail.com>
2022-05-14 09:54:46 +0200

    single setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install homebrew package manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install packages
+brew bundle
+
+# osx system defaults
+./defaults/system.sh
+
+# Add the fish to the list of legit shells
+sudo bash -c "echo /usr/local/bin/fish >> /private/etc/shells"
+
+
 
 # exclude directories from Time Machine backups
 tmutil addexclusion ~/Downloads
@@ -11,14 +25,11 @@ mkdir -p ~/Frameworks/go/bin
 
 # Project diretories
 mkdir -p ~/Projects/devops
-mkdir -p ~/Projects/external
-
-# Create Wiki
-mkdir -p ~/Documents/Wiki
+mkdir -p ~/Projects/oliver
 
 # Setup notes
-git clone https://github.com/oschrenk/notes.git $HOME/Documents/Notes
-cd $HOME/Documents/Notes
+git clone https://github.com/oschrenk/notes.git $HOME/Projects/oliver/notes
+cd $HOME/Projects/oliver/notes
 git config --local user.email oliver.schrenk@gmail.com
 cd $HOME
 
@@ -31,10 +42,6 @@ rbenv global $LAST_RUBY_VERSION
 
 # switch back to install dir
 cd $DIR
-
-# setup user bins
-./installs/go.sh
-./installs/npm.sh
 
 # homebrew
 # disable analytics
@@ -54,9 +61,21 @@ brew analytics off
 cp ./agents/* ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/*.plist
 
-
 # Setup fish as default shell
 # Change the shell for the user
 sudo chsh -s /usr/local/bin/fish
 
+# setup dotfiles
+git clone https://github.com/oschrenk/dotfiles $HOME/.tilde/dotfiles
+cd $HOME/.tilde/dotfiles
+git config --local user.email oliver.schrenk@gmail.com
+
+source $HOME/.tilde/dotfiles/.config/fish/functions/tilde.fish
+tilde link dotfiles
+
+source $HOME/.tilde/dotfiles/.config/fish/functions/append-to-path.fish
+source $HOME/.tilde/dotfiles/.config/fish/functions/prepend-to-path.fish
+
+# setup env
+source $HOME/.tilde/dotfiles/.config/fish/env.fish
 
