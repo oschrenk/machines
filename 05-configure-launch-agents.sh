f14655ae6571931d9ea045652aa2cf87aec55199
Oliver Schrenk <oliver.schrenk@gmail.com>
2022-05-15 11:28:48 +0200

    Fix launch agents loading

--- 05-configure-launch-agents.sh
+++ 05-configure-launch-agents.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
 # launch agents
-cp ./modules/agents/* ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/*.plist
+for FILE in ./modules/agents/*; do
+  # copy it to user's library
+  cp $FILE $HOME/Library/LaunchAgents
+
+  # enable it
+  # we rely on the filename being the name of the service
+  # first cut removes ./modules/agents prefix
+  # seond cut removes .plist postfix
+  SERVICE_NAME=$(echo $FILE | cut -d '/' -f 4- | rev | cut -d '.' -f 2- | rev)
+  USER_ID=$(id -u)
+  TARGET_SPECIFIER="gui/$USER_ID/$SERVICE_NAME"
+  echo "Enabling $SERVICE_NAME"
+  launchctl enable $TARGET_SPECIFIER
+done
+# launchctl load $HOME/Library/LaunchAgents/*.plist
