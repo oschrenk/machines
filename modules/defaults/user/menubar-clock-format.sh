22b3578ad744e0461451fc95d32d258e106ea7a3
Oliver Schrenk <oliver.schrenk@gmail.com>
2022-05-15 15:04:15 +0200

    menubar clock format

---
+++ modules/defaults/user/menubar-clock-format.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Set Date/Time Format in Menubar like 17. Apr 17:58
+defaults write com.apple.menuextra.clock DateFormat -string "d. MMM  HH:mm"
--- modules/defaults/user/unvalidated.sh
+++ modules/defaults/user/unvalidated.sh
@@ -102,9 +102,6 @@ defaults write NSGlobalDomain _HIHideMenuBar -bool true
 # Disable menu bar transparency
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 
-# Set Date/Time Format in Menubar like 17. Apr 17:58
-defaults write com.apple.menuextra.clock DateFormat -string "d. MMM  HH:mm"
-
 # Show fast user switching menu as: 0: Full Name, 1: Short Name, 2: Icon
 defaults write -g userMenuExtraStyle -int 1
 
